366601989f61f302773090367def2817ff0ef22f
ALEXANDER FIGUEREDO <alexanderfigueredo.gms@gmail.com>
2024-09-11 20:00:22 -0300

    fix cambio de ruta en servir arcivo statico

--- tecprendimientoAPI/app.js
+++ tecprendimientoAPI/app.js
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors())
-app.use('/uploads', express.static( __dirname + '/src/uploads'));
+// app.use('/uploads', express.static( __dirname + '/src/uploads'));
+app.use('/uploads', express.static('/app/src/uploads'));
 app.use(fileupload({createParentPath: true}));
 app.use(express.json());
 app.use(express.text());
@@ -19,7 +20,8 @@ app.use(router);
 // endpoints y metodo para servir las imagenes
 router.get("/uploads/:nameImg", (req, res) => {
   const { nameImg } = req.params;
-  return res.sendFile(path.join(__dirname, `../src/uploads/${nameImg}`));
+  return res.sendFile(path.join("/app", `/src/uploads/${nameImg}`));
+  // return res.sendFile(path.join(__dirname, `../src/uploads/${nameImg}`));
 });
 
 
